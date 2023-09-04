eae7422c326aee103bbe5afe02fd79e9ff6888ba
CHANYOUNKIM <hayoun124@naver.com>
2023-09-04 13:42:12 +0900

    week1_1

--- week1_1/FivonacciPrint.java
+++ week1_1/FivonacciPrint.java
@@ -8,13 +8,14 @@ public class FivonacciPrint {
         int second = 1;
         int next = 0;
         int sum = 0;
+        int i = 1;
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("피보나치 수열의 마지막 항을 입력하시오 : ");
         n = input.nextInt();
 
-        for(int i = 1; i <= n; i++)
+        while(true)
         {
             if(i <= 2)
             {
@@ -33,6 +34,9 @@ public class FivonacciPrint {
             {
                 System.out.print(", ");
             }
+            i++;
+            if(i > n)
+                break;
         }
         System.out.println("\n수열 합계 : " + sum);
     }
