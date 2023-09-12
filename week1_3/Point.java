a78d24a62c1f85902a4fe3f23cb1a7bebd96e8a5
CHANYOUNKIM <hayoun124@naver.com>
2023-09-12 10:32:41 +0900

    week1-3

---
+++ week1_3/Point.java
@@ -0,0 +1,13 @@
+package week1_3;
+
+public class Point {
+    // 멤버 변수 - 인스턴스 변수 - x
+    // 정적 변수 - 클래스 변수 - static
+    // 멤버 메서드 - 인스턴스 메서드 - x
+    // 정적 메서드 - 클래스 메서드 - static
+
+    int posX; // x좌표 값을 저장
+    int posY; // y좌표 값을 저장
+
+
+}
